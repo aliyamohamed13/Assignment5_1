cf3776b5219a2416c14fd735fe93ddbefb29e203
alimo13 <aliyamohamed13@gmail.com>
2019-07-15 21:00:50 -0700

    changed package.json postbuild and put if statement back

--- app.js
+++ app.js
@@ -98,13 +98,13 @@ let j = bodyParser.json();
 //   res.json(messages);
 // });
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static( 'client/build' ));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path
   });
-// }
+}
 
 app.listen(port, (req, res) => {console.log("server for app started on " + {port})})
 
